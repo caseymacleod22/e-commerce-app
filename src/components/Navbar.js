4d4922fd3124d5fb1786420b41391e420d1fdfce
Casey MacLeod <caseymacleod22@gmail.com>
2021-12-15 11:53:33 -0500

    adds input box,m search icon, site name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,29 +4,45 @@ import styled from 'styled-components'
 
 const Container = styled.div`
     height: 60px;
-`
+`;
 
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
+    align-items: center;
     justify-content: space-between;
-`
+`;
 
 const Left = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
 `;
 
 const Language = styled.span`
     font-size: 14px;
     cursor: pointer;
-`
+`;
 
 const SearchContainer = styled.div`
     border: 1px solid lightgray;
-`
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+
+const Input = styled.input`
+    border: none;
+`;
 
 const Center = styled.div`
     flex: 1;
+    text-align: center;
+`;
+
+const Logo = styled.h1`
+    font-weight: bold;
 `;
 const Right = styled.div`
     flex: 1;   
@@ -39,11 +55,13 @@ const Navbar = () => {
                 <Left>
                     <Language>EN</Language>
                     <SearchContainer>
-                        input
+                        <Input />
                         <Search />
                     </SearchContainer>
                 </Left>
-                <Center>Center</Center>
+                <Center>
+                    <Logo>shop stop</Logo>
+                </Center>
                 <Right>Right</Right>
             </Wrapper>
         </Container>
