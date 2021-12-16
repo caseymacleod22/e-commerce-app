7e74978f3caff997f0d026a7dcbbf17bf0910916
Casey MacLeod <caseymacleod22@gmail.com>
2021-12-16 17:25:35 -0500

    adds responsive design to all pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
 import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from "@material-ui/icons"
 import styled from "styled-components"
+import { mobile } from "../Responsive"
 
 const Container = styled.div`
     display: flex;
+    ${mobile({flexDirection: 'column'})}
 `
 const Left = styled.div`
     flex: 1;
@@ -33,6 +35,7 @@ const SocialIcon = styled.div`
 const Center = styled.div`
     flex: 1;
     padding: 20px;
+    ${mobile({display: 'none'})}
 `
 
 const Title = styled.h3`
@@ -56,6 +59,7 @@ const ListItem = styled.li`
 const Right = styled.div`
     flex:1;
     padding: 20px;
+    ${mobile({backgroundColor: '#fff8f8'})}
 `
 
 const ContactItem = styled.div`
