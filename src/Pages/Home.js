981c32604784036a5349a5542f3d60b125fb467a
Casey MacLeod <caseymacleod22@gmail.com>
2022-01-02 23:16:19 -0500

    imports Announcements.js in Home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,7 +10,7 @@ import Footer from '../components/Footer'
 const Home = () => {
     return (
         <div>
-            {/* <Announcements /> */}
+            <Announcements />
             <Navbar />
             <Slider />
             <Categories />
